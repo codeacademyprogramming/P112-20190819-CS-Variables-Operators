03f770db031af34699356cc76d35abf7cadd08f7
Sattar Amrahov <sattar@code.local>
2019-08-19 12:02:16 +0400

    files added

---
+++ ConsoleDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleDemo
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // int num1 = 5;
+            int num2 = 10;
+            /*Console.WriteLine(num1 + " " + num2);
+            Square(num1, num2);
+            Console.WriteLine(num1 + " " + num2); */
+
+            Console.ReadLine();
+        }
+
+        static void Square(int a, int b)
+        {
+            a = a * a;
+            b = b * b;
+            Console.WriteLine(a + " " + b);
+        }
+    }
+}
